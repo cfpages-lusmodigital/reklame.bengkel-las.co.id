1f1d92f08c3d73543af027d0d7ef4ae5fd516f89
Syakirana28 <110509134+Syakirana28@users.noreply.github.com>
2024-05-28 12:47:50 +0700

    upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-18.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ff102d9c' );
+});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"97d888c0","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"email@reklame.bengkel-las.co.id","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-97d888c0', 18 );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-9.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ff102d9c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e70e2b79' );
+});
+ });
